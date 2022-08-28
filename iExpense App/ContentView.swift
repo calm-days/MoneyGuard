8b44ce7d60c5d89fe5743ae2ad2e53318d72fd2f
Roman Liukevich <r.liukevich@gmail.com>
2022-08-28 08:02:39 +0200

    Initial Commit

---
+++ iExpense App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense App
+//
+//  Created by Roman Liukevich on 8/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense App/iExpense_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_AppApp.swift
+//  iExpense App
+//
+//  Created by Roman Liukevich on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
